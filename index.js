ecb784a227000c417f79e01d54ba8067333efa8a
Thai Duy Roan <128533558+tdr8012@users.noreply.github.com>
2025-07-22 19:32:35 -0400

    added css styles

---
+++ index.js
@@ -0,0 +1,7 @@
+const weatherForm = document.querySelector(".weatherForm");
+
+
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card"); //give me the first element with the class card
+const apiKey = "d7c6d84bafbd9a28bac90fad59fb66ba";
+; 
